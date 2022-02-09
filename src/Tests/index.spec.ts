9cb8a06bc6e6420c0cab20cb4136ec81523458b7
João Paulo Kuwano <72429644+joaopaulo-kuwano@users.noreply.github.com>
2022-02-09 07:53:26 -0300

    build: teste de exemplo

---
+++ src/Tests/index.spec.ts
@@ -0,0 +1,8 @@
+// eslint-disable-next-line no-undef
+describe('Setup Jest + TS', () => {
+  // eslint-disable-next-line no-undef
+  test('criando asserção', () => {
+    // eslint-disable-next-line no-undef
+    expect(1 + 1).toBe(2)
+  })
+})
