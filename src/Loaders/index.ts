06f04789b21808ccffa4b3e2f92a174f1cb01ef0
João Paulo Kuwano <72429644+joaopaulo-kuwano@users.noreply.github.com>
2022-03-28 19:26:43 -0300

    chore: remove credencials aws

--- src/Loaders/index.ts
+++ src/Loaders/index.ts
@@ -1,5 +1,4 @@
 import { ApiLoader } from '../Api'
-import { Pool } from 'pg'
 
 export class AppLoader {
   constructor () {
@@ -7,9 +6,5 @@ export class AppLoader {
     api.setupMiddlewares()
     api.setupRoutes()
     api.initServer()
-
-    const endpointUrl = 'dynamodb.us-east-1.amazonaws.com'
-    const accessKey = 'AKIAQ5WA5PTGBZW6GWOD'
-    const secretKey = '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'
   }
 }
