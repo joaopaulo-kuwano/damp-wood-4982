33e828ffd80d32d0b0dcf633497d068366d86c6e
João Paulo Kuwano <72429644+joaopaulo-kuwano@users.noreply.github.com>
2022-03-29 06:53:28 -0300

    feat: repositorio de cat produto em supabase

--- src/Repositories/ProductCategory.ts
+++ src/Repositories/ProductCategory.ts
@@ -1,14 +1,21 @@
 import { IProductCategory } from '../Entities/ProductCategory'
 
+export interface RepoError {
+  code: string
+  details: string
+  hint: string
+  message: string
+}
+
 export interface ListParams {
-  nid?: string
-  companyNid?: string
+  id?: string
+  companyId?: string
   withProducts?: boolean
 }
 
 export interface IProductCategoryRepo {
-  add: (form: IProductCategory) => Promise<{ data: any, error: any }>
-  list: (params: ListParams) => Promise<{ data: IProductCategory[], error: any }>
-  update: () => Promise<{ data: any, error: any }>
-  delete: (e: string) => Promise<{ data: any, error: any }>
+  add: (form: IProductCategory) => Promise<{ data: IProductCategory[], error: RepoError }>
+  list: (params: ListParams) => Promise<{ data: IProductCategory[], error: RepoError }>
+  // update: () => Promise<{ data: any, error: any }>
+  // delete: (e: string) => Promise<{ data: any, error: any }>
 }
---
+++ src/Repositories/ProductCategorySupa.ts
@@ -0,0 +1,30 @@
+import { SupabaseClient } from '@supabase/supabase-js'
+import { IProductCategory } from '../Entities/ProductCategory'
+import { IProductCategoryRepo, ListParams, RepoError } from './ProductCategory'
+
+export class ProductCategorySupaRepo implements IProductCategoryRepo {
+  // eslint-disable-next-line no-useless-constructor
+  constructor (private client: SupabaseClient) { }
+
+  async add (form: IProductCategory): Promise<{ data: IProductCategory[], error: RepoError }> {
+    const api = await this.client.from('ProductCategory').insert(form)
+    return { data: api.data as any, error: api.error as any }
+  }
+
+  async list (params: ListParams) {
+    let select = '*'
+    // eslint-disable-next-line prefer-const
+    let match = {}
+    if (!params.withProducts) {
+      select = 'id, oid, name, active, createdAt, updatedAt, companyId'
+    }
+    if (params.id) {
+      Object.assign(match, { id: params.id })
+    }
+    if (params.companyId) {
+      Object.assign(match, { companyId: params.companyId })
+    }
+    const api = await this.client.from('ProductCategory').select(select).match(match)
+    return { data: api.data as any, error: api.error as any }
+  }
+}
